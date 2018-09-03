3b52f5a54ea633a68228ac0fabfa5aaca254bd0c
papb <papb1996@gmail.com>
2018-09-02 23:33:45 -0300

    Logs: add persisting logs (see #1)

--- index.js
+++ index.js
@@ -2,22 +2,25 @@
 
 const server = require("./lib/server");
 const db = require("./lib/db");
+const log = require("./lib/log");
 const jsonifyError = require("jsonify-error");
 const stringifyError = error => JSON.stringify(jsonifyError(error), null, 4);
 
 /* eslint no-process-exit:off */
+
+log("Starting server...");
 
 db.setup().then(() => {
     const port = 8000;
     return Promise.resolve().then(() => {
         return server.start(port);
     }).then(() => {
-        console.log(`Server is listening on port ${port}.`);
+        log(`Server is listening on port ${port}.`);
     }, error => {
-        console.error(`Failed to start server on port ${port}:`, stringifyError(error));
+        log(`Error: Failed to start server on port ${port}: ${stringifyError(error)}`);
         process.exit(1);
     });
 }, error => {
-    console.error(`Failed to setup database:`, stringifyError(error));
+    log(`Error: Failed to setup database: ${stringifyError(error)}`);
     process.exit(1);
 });
